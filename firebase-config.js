58723ec463817facec3be2c0016f56988a2c4fff
blumark24 <blumark24@gmail.com>
2025-10-27 17:46:47 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,5 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
+// إعدادات مشروع Smart HSR Owner
+export const firebaseConfig = {
   apiKey: "AIzaSyCliX4lQa7YWp0unYFp6-rZnGIgwC6MIQk",
   authDomain: "smart-hsr-owner.firebaseapp.com",
   projectId: "smart-hsr-owner",
@@ -12,6 +7,3 @@ const firebaseConfig = {
   messagingSenderId: "234872814790",
   appId: "1:234872814790:web:66e1be939d0c300e3a6bc7"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
