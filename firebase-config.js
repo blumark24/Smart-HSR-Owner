582eb30396e7846264b11673e56b92411e440a8e
blumark24 <blumark24@gmail.com>
2025-10-27 17:43:00 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,17 @@
-export const firebaseConfig = {
-  apiKey: "REPLACE_WITH_API_KEY",
-  authDomain: "REPLACE_WITH_PROJECT_ID.firebaseapp.com",
-  projectId: "REPLACE_WITH_PROJECT_ID",
-  storageBucket: "REPLACE_WITH_PROJECT_ID.appspot.com",
-  messagingSenderId: "REPLACE_WITH_SENDER_ID",
-  appId: "REPLACE_WITH_APP_ID"
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCliX4lQa7YWp0unYFp6-rZnGIgwC6MIQk",
+  authDomain: "smart-hsr-owner.firebaseapp.com",
+  projectId: "smart-hsr-owner",
+  storageBucket: "smart-hsr-owner.firebasestorage.app",
+  messagingSenderId: "234872814790",
+  appId: "1:234872814790:web:66e1be939d0c300e3a6bc7"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
